a5dfd55ead1d27f6ee65a6a28058ef790f7aa293
top <1390276254@qq.com>
2020-08-07 22:36:13 +0800

    集群配置更改，服务名称以及ip的配置

--- order/src/main/java/org/top/coonstant/UrlConstant.java
+++ order/src/main/java/org/top/coonstant/UrlConstant.java
@@ -2,5 +2,9 @@ package org.top.coonstant;
 
 public interface UrlConstant {
 
-    public static final  String PAYMENT_URL = "http://localhost:8081";
+    // 服务单机版
+    // public static final  String PAYMENT_URL = "http://localhost:8081";
+
+    //服务集群版
+    public static final  String PAYMENT_URL = "http://PAY-PROVIDER";
 }
