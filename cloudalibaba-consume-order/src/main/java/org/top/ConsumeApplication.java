2549b9021da5d68ab428b2b330edda30b0309d2a
top <1390276254@qq.com>
2022-05-10 22:40:06 +0800

    nacos之服务提供者注册和服务消费者注册及负载均衡

---
+++ cloudalibaba-consume-order/src/main/java/org/top/ConsumeApplication.java
@@ -0,0 +1,14 @@
+package org.top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeApplication.class, args);
+    }
+}
